b7533b7e54044b7fc046a2e8e11d9fdc231a8ff7
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-11-30 10:26:11 -0800

    added authenicate_users allowing only welcome page

--- app/controllers/savers_controller.rb
+++ app/controllers/savers_controller.rb
@@ -1,7 +1,27 @@
 class SaversController < ApplicationController
 	def welcome
 	end
-	
+
 	def index
+		@savers = Saver.all
 	end
+
+	def new
+		@saver = Save.new
+	end
+
+	def create
+		saver = current_user.savers.new(savers_params)
+		if saver.save
+			redirect_to save.id 
+		else
+			render 'new'
+		end	
+	end
+
+	private
+
+	def savers_params
+		params.require(:saver).permit(:gas, :kwh)
+	end		
 end
