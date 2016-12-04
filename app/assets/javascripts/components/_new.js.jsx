97eddf6b5c5f7c605f388893b72137cb0537b0f7
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-04 14:32:36 -0800

    basic form is working

--- app/assets/javascripts/components/_new.js.jsx
+++ app/assets/javascripts/components/_new.js.jsx
@@ -1,7 +1,23 @@
 var New = React.createClass({
+	
+
+	handleClick() {
+		var kwh = this.refs.kwh.value;
+    var gas = this.refs.gas.value;
+    var address = this.refs.address.value;
+
+  $.ajax({
+    url: '/savers',
+    type: 'POST',
+    data: { saver: { kwh: kwh, gas: gas, address: address } },
+    success: (response) => {
+      console.log('it worked!', response);
+    }
+  });
+},
   render() {
     return (
-      <div className=>
+      <div>
         <h1>Add your gas and electric usage here</h1>
         <input ref="kwh" placeholder="Kwh per month" />
     		<input ref="gas" placeholder="Gas per month" />
