7293aaec771d3f995d4917ea0fbc3d7d766de65f
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-08 05:55:17 -0800

    fixing fb graph api

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_many :savers       
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
+  has_many :savers   
+
+  def self.from_omniauth(auth)
+    binding.pry
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.oauth_token = auth.credentials.token
+        user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+        user.password = Devise.friendly_token[0,20]
+    end
+  end  
+
+  def self.facebook(auth)
+    @facebook ||= Koala::Facebook::API.new(oauth_access_token)
+  end  
 end
+
