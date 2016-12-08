705cb9a3a2b40e3ccc6d3130bfeed4a7b35a8fd1
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-08 13:08:47 -0800

    added conditional to try to fix the problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,19 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     binding.pry
-      where(provider: auth.provider, uid: auth.uid, oauth_token: auth.credentials.token).first_or_create do |user|
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.provider = auth.provider
         user.uid = auth.uid
-        # if !user.email.empty?
-        #     user.email
-        # else
-        #     user.email = auth.info.email
-        # user.oauth_token = auth.oauth_token
+        if user.email.empty?
+           user.email = auth.info.email
+            
+        else
+            user.email 
+
+        user.oauth_token = auth.credentials.token
         @facebook = Koala::Facebook::API.new(auth.credentials.token)
         user.password = Devise.friendly_token[0,20]
+      end
     end
   end  
 
