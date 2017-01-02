949aecaa0cabc4e5e0e122d20c156fca19b3b669
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-02 15:50:47 -0800

    changed order

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" } 
-  root 'savers#welcome'
-	resources :savers,only:[:index,:create,:new, :show]
-	get 'savers', to: 'savers#about'
+  get "savers/about" 
+  root "savers#welcome"
+	resources :savers
 end
