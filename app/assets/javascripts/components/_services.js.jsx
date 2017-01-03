42300035924255587b776cba3d7272c6686b6435
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2017-01-03 09:57:03 -0800

    adding services compoent

---
+++ app/assets/javascripts/components/_services.js.jsx
@@ -0,0 +1,14 @@
+var Services = React.createClass({
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron">
+	        <h1><u>Our Services</u></h1>
+	         <div className="text-center">
+	         
+	         </div>
+	         </div>
+	      </div>
+      )
+    }
+});
